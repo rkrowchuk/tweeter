edbc6a6bc3f49931904f2f245f773e4b9917f734
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-03-01 20:18:09 -0800

    formatting and refactoring

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,12 +2,7 @@ $(document).ready(function() {
  
   $('.tweet-input').keyup(function() {
     $('.counter').text(140 - $(this).val().length);
-    if ($(this).val().length > 140) {
-      $('.counter').addClass('negative');
-    }
-    if ($(this).val().length < 140) {
-      $('.counter').removeClass('negative');
-    }
+    $(this).val().length > 140 ? $('.counter').addClass('negative') : $('.counter').removeClass('negative');
   });
 
 });
