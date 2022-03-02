bf0fa1c13e3296eb8ae152aafe7a055086f7c07f
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-03-02 14:47:14 -0800

    implement renderTweets function for adding tweets to the page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,48 +3,82 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
 $(document).ready(function() {
-const createTweetElement = function(obj) {
 
-  const $tweet = $(
-  `<article class="tweet">
-  <header>
-      <div id="user">
-        <img src=${tweetData["user"]["avatars"]}/>
-        <h5>${tweetData["user"]["name"]}</h5>
-      </div>
-      <h5 id="handle">${tweetData["user"]["handle"]}</h5>
-    </header>
-  <p>${tweetData["content"]["text"]}</p>
-  <footer>
-      <p>${tweetData["created_at"]}</p>
-      <div id="icons">
-        <i class="fa-solid fa-flag fa-2xs"></i>
-        <i class="fa-solid fa-retweet fa-2xs"></i>
-        <i class="fa-solid fa-heart fa-2xs"></i>
-      </div>
-    </footer>
-  </article>`);
-
-  return $tweet;
-}
-
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
-const $tweet = createTweetElement(tweetData);
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    for (let tweet of tweets) {
+      // calls createTweetElement for each tweet
+      let $newTweet = createTweetElement(tweet);
+      // takes return value and appends it to the tweets container
+      $('.tweet-container').append($newTweet);
+    }
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('.tweet-container').append($tweet);
+  }
+
+  const createTweetElement = function(tweet) {
+
+    let $tweet = $(
+    `<article class="tweet">
+    <header>
+        <div id="user">
+          <img src=${tweet["user"]["avatars"]}/>
+          <h5>${tweet["user"]["name"]}</h5>
+        </div>
+        <h5 id="handle">${tweet["user"]["handle"]}</h5>
+      </header>
+    <p>${tweet["content"]["text"]}</p>
+    <footer>
+        <p>${timeago.format(tweet["created_at"])}</p>
+        <div id="icons">
+          <i class="fa-solid fa-flag fa-2xs"></i>
+          <i class="fa-solid fa-retweet fa-2xs"></i>
+          <i class="fa-solid fa-heart fa-2xs"></i>
+        </div>
+      </footer>
+    </article>`);
+
+    return $tweet;
+  }
+
+// const tweetData = {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@SirIsaac"
+//     },
+//   "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//   "created_at": 1461116232227
+// }
+
+renderTweets(data);
 
 });
